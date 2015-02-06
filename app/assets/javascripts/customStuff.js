ae9ee99c75e93511d4b73572bd8930d8ed107aac
rnjai <rnjailamba12@gmail.com>
2015-02-06 21:53:38 +0530

    Form responsiveness to small screen

--- app/assets/javascripts/customStuff.js
+++ app/assets/javascripts/customStuff.js
@@ -1,3 +1,5 @@
+
+
 var j = jQuery.noConflict();
 j(document).ready(function(){
     j('#datetimepicker4').datetimepicker();
@@ -8,14 +10,25 @@ j(document).ready(function() {
         // options
         itemSelector : '.masonry-item',
         // options...
-    isFitWidth: true
+    	isFitWidth: true
 
     });
 });
 
 
-
-
 j(function(){
   j(".Fname").focus();
+});
+
+j(function() {
+  var flashCallback;
+  flashCallback = function() {
+    return j(".flash-message").fadeOut();
+  };
+  j(".flash-message").bind('click', (function(_this) {
+    return function(ev) {
+      return j(".flash-message").fadeOut();
+    };
+  })(this));
+  return setTimeout(flashCallback, 3000);
 });
