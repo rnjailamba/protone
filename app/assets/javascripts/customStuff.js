f162b71cc71e3180af2ef649b06a05814e7dcae8
rnjai <rnjailamba12@gmail.com>
2015-02-11 01:33:18 +0530

    Landing page done in principle,css files trimmed,fa fa icons

--- app/assets/javascripts/customStuff.js
+++ app/assets/javascripts/customStuff.js
@@ -107,6 +107,18 @@ j(document).ready(function() {
     });
 });
 
+
+/*!
+ * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+
+
+// Floating label headings for the contact form
+
     
 
 
